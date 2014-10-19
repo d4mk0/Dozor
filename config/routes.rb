6a54434787b0d147c6c224ebe4fe815a4fdb20be
Damir Konkov <d4mk0f@gmail.com>
2014-10-19 14:10:48 +0400

    good routes was added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  root to: 'login#login'
+  root to: 'sessions#login'
+  resources :quests do
+    member do
+      get :expected
+      get :complete
+      get :results
+    end
+    resources :tasks do
+      post 'attempt', on: :member
+    end
+  end
+
+  post '/login', to: 'sessions#create'
+  delete '/login', to: 'sessions#destroy'
 end
